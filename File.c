dca8fc2906a27c6e8c05b54117df2c2407202145
Artem <112772405+ed1skrad@users.noreply.github.com>
2023-05-07 00:21:15 +0300

    sass

--- File.c
+++ File.c
@@ -18,7 +18,6 @@ FILE* openFile(const char* filename) {
 int fileConverting(const char* filename, struct WordDecompress** wordDec, int* numberOfWords, char*** words, int* wordsSize) {
     FILE *file = openFile(filename);
 
-
     char* str = NULL;
     int currSize = 0;
     char c;
@@ -32,20 +31,39 @@ int fileConverting(const char* filename, struct WordDecompress** wordDec, int* n
 
         if(ln == 0) {
             if(c != ' ' && c != '\n') {
-                str = realloc(str, (currSize+1)*sizeof(char));
+                char *tmp = realloc(str, (currSize+1)*sizeof(char));
+                if (tmp == NULL) {
+                    free(str);
+                    fclose(file);
+                    return 0;
+                }
+                str = tmp;
                 str[currSize] = c;
                 currSize++;
                 continue;
             }
 
             currSize++;
-            str = realloc(str, currSize*sizeof(char));
+            char *tmp = realloc(str, currSize*sizeof(char));
+            if (tmp == NULL) {
+                free(str);
+                fclose(file);
+                return 0;
+            }
+            str = tmp;
             str[currSize-1] = '\0';
 
             if(*wordDec == NULL || (*wordDec)[*numberOfWords-1].secondWord != NULL)
             {
                 (*numberOfWords)++;
-                *wordDec = realloc(*wordDec, (*numberOfWords)*sizeof(struct WordDecompress));
+                struct WordDecompress *tmp = realloc(*wordDec, (*numberOfWords)*sizeof(struct WordDecompress));
+                if (tmp == NULL) {
+                    free(*wordDec);
+                    free(str);
+                    fclose(file);
+                    return 0;
+                }
+                *wordDec = tmp;
                 (*wordDec)[*numberOfWords-1].firstWord = NULL;
                 (*wordDec)[*numberOfWords-1].secondWord = NULL;
             }
@@ -63,36 +81,88 @@ int fileConverting(const char* filename, struct WordDecompress** wordDec, int* n
         }
         else {
             if(punctuationMark(c) == 0) {
-                str = realloc(str, (currSize+1)*sizeof(char));
+                char *tmp = realloc(str, (currSize+1)*sizeof(char));
+                if (tmp == NULL) {
+                    free(str);
+                    fclose(file);
+                    return 0;
+                }
+                str = tmp;
                 str[currSize] = c;
                 currSize++;
             }
             else {
                 currSize++;
-                str = realloc(str, currSize*sizeof(char));
+                char *tmp = realloc(str, currSize*sizeof(char));
+                if (tmp == NULL) {
+                    free(str);
+                    fclose(file);
+                    return 0;
+                }
+                str = tmp;
                 str[currSize-1] = '\0';
 
-                *words = realloc(*words, ((*wordsSize) + 1) * sizeof(char *));
+                char **tmp_words = realloc(*words, ((*wordsSize) + 1) * sizeof(char *));
+                if (tmp_words == NULL) {
+                    free(*words);
+                    free(str);
+                    fclose(file);
+                    return 0;
+                }
+                *words = tmp_words;
+
                 (*words)[*wordsSize] = strdup(str);
+
+                if ((*words)[*wordsSize] == NULL) {
+                    free(*words);
+                    free(str);
+                    fclose(file);
+                    return 0;
+                }
+
                 (*wordsSize)++;
 
                 str = calloc(2, sizeof(char));
+
+                if (str == NULL) {
+                    free(*words);
+                    fclose(file);
+                    return 0;
+                }
+
                 str[0] = c;
                 str[1] = '\0';
 
-                *words = realloc(*words, ((*wordsSize) + 1) * sizeof(char *));
+                tmp_words = realloc(*words, ((*wordsSize) + 1) * sizeof(char *));
+
+                if (tmp_words == NULL) {
+                    free(*words);
+                    free(str);
+                    fclose(file);
+                    return 0;
+                }
+
+                *words = tmp_words;
+
                 (*words)[*wordsSize] = str;
+
                 (*wordsSize)++;
 
                 str = NULL;
+
                 currSize = 0;
+
             }
+
         }
+
     }
 
     fclose(file);
+
     free(str);
 
     return 1;
+
 }
 
