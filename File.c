137271bc4221704f3d9b0f4fe3b04071a2808b86
Artem <112772405+ed1skrad@users.noreply.github.com>
2023-05-09 16:07:20 +0300

    sss

--- File.c
+++ File.c
@@ -74,8 +74,7 @@ int fileConverting(const char* filename, struct WordDecompress** wordDec, int* n
                 *words = realloc(*words, ((*wordsSize) + 1) * sizeof(char *));
                 (*words)[*wordsSize] = strdup(str);
                 (*wordsSize)++;
-
-                free(str);
+                
 
                 str = calloc(2, sizeof(char));
                 str[0] = c;
@@ -95,7 +94,6 @@ int fileConverting(const char* filename, struct WordDecompress** wordDec, int* n
 
     if (str != NULL) {
         free(str);
-        str == NULL;
     }
 
     return 1;
